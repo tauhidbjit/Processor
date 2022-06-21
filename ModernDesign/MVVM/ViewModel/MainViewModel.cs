3abb41f41a746c1c419c62dc32a48e9cf7ab4567
Md. Tauhid Uz Zaman <tauhid.zaman@bjitgroup.com>
2022-06-21 19:20:05 +0600

    Practiced sample designs found on youtube.

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeViewModel { get; set; }
+        public DiscoverViewModel DiscoveryViewModel { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeViewModel = new HomeViewModel();
+            DiscoveryViewModel = new DiscoverViewModel();
+
+            _currentView = HomeViewModel;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeViewModel;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryViewModel;
+            });
+        }
+    }
+}
--- ModernDesign/MainWindow.xaml.cs
+++ ModernDesign/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace InventoryApp
+namespace ModernDesign
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,14 +23,6 @@ namespace InventoryApp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void addProductInfoButton_Click(object sender, RoutedEventArgs e)
-        {
-            spinnerUserControl.Visibility = Visibility.Visible;
-            AddProductInfoWindow addProductInfoWindow = new AddProductInfoWindow();
-            addProductInfoWindow.ShowDialog();
-            spinnerUserControl.Visibility = Visibility.Hidden;
         }
     }
 }
